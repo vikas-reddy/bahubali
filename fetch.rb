f8609655e2d44611e34e0ab8705f30a2b32dfaed
Vikas Reddy <vikasreddy.com@gmail.com>
2017-04-25 11:38:17 +0530

    initial commit

---
+++ fetch.rb
@@ -0,0 +1,87 @@
+require 'open-uri'
+require 'nokogiri'
+require 'net/smtp'
+
+INTERVAL = 120
+
+# Websites
+BOOKMYSHOW = {
+  name: "BookMyShow",
+  url: "https://in.bookmyshow.com/buytickets/baahubali-2-the-conclusion-hyderabad/movie-hyd-ET00038693-MT/20170429",
+  selector: "ul#venuelist li.list div.details a.__venue-name strong"
+}
+JUSTICKETS = {
+  name: "Justickets",
+  url: "https://data.justickets.co/datapax/JUSTICKETS.hyderabad.baahubali-2-the-conclusion.v1.json",
+  selector: "div.schedule-listing div.info div.theatre"
+}
+
+# Theaters
+THEATERS = [
+  'BVK',
+  'Miraj'
+]
+
+# Email configuration
+NAME_FROM = 'Vikas Reddy'
+EMAIL_FROM = 'vikasreddy.com@gmail.com'
+EMAIL_TO = 'vikasreddy.com@gmail.com'
+PWD = 'THE_ORIGINAL_PASSWORD'
+
+
+def formatted_message(content)
+<<-MSG
+From: Vikas Program <vikas@reddy.com>
+To: Vikas Reddy <vikasreddy.com@gmail.com>, Swathi <swati.blues@gmail.com>
+Content-Type: text/html
+Subject: Bahubali booking started!
+
+#{content}
+MSG
+end
+
+def send_email(message)
+  smtp = Net::SMTP.new('smtp.gmail.com', 587)
+  smtp.enable_starttls
+  smtp.start('mail.google.com', EMAIL_FROM, PWD, :login) do
+    # puts formatted_message(message)
+    smtp.send_message(formatted_message(message), EMAIL_FROM, EMAIL_TO)
+  end
+end
+
+def check(website)
+  url = website[:url]
+  doc = Nokogiri::HTML(open(url))
+
+  message = ''
+  doc.css(website[:selector]).map(&:text).each do |name|
+    THEATERS.each do |theater|
+      # puts "Matching #{theater} with #{name}"
+      r = Regexp.new(theater, 'i')
+      if r.match(name)
+        if message.empty?
+          message << "<p> <a href=\"#{website[:url]}\">BookMyShow</a> </p>"
+        end
+        message << "<div>" << name << "</div>"
+      end
+    end
+  end
+
+  if !message.empty?
+    puts "Sending email about: "
+    puts message
+    send_email(message)
+  else
+    puts "Nothing found"
+  end
+end
+
+
+while true
+  # puts check(JUSTICKETS)
+  puts "Started at #{Time.now}"
+  check(BOOKMYSHOW)
+  puts "Ended at #{Time.now}"
+  puts "... sleeping for #{INTERVAL} seconds\n\n"
+  sleep INTERVAL
+end
